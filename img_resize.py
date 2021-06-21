f9159dcbbe1dd3af9719a9ae016b1063a9a10ea3
klyshnikov <86251037+klyshnikov@users.noreply.github.com>
2021-06-21 21:32:54 +0500

    Create img_resize.py

---
+++ img_resize.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import json
+
+
+def main_resize(user_id):
+    with open('data_file.json', 'r') as j:
+        json_data = json.load(j)
+
+    imsize = int(json_data[user_id]["im_size"])
+
+    content_img = Image.open(user_id + "content.jpg")
+    style_img = Image.open(user_id + "style.jpg")
+
+    content_img = content_img.resize((imsize, imsize))
+    style_img = style_img.resize((imsize, imsize))
+
+    content_img.save(user_id + 'content_sqr.jpg')
+    style_img.save(user_id + 'style_sqr.jpg')
